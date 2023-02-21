ad4bd7e93e84ae3a7c6bfa54e8c7499d3fd16cc0
Olumide <o3cloudng@gmail.com>
2023-02-21 16:54:28 +0100

    static root config

--- core/settings.py
+++ core/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-5en^m6=6pd9d75x%&ayvuq$^*4x9qm_cf4xxpu4w#0a0r_1=i8' #config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True # config('DEBUG', cast=bool)
