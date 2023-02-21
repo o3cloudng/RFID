56a6267dc0355ca7c561fa562bfa7f9174554a6a
Olumide <o3cloudng@gmail.com>
2023-02-21 17:34:01 +0100

    Dashboard link

--- core/urls.py
+++ core/urls.py
@@ -4,8 +4,10 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('dashboard.urls')),
     path('api-auth/', include('rest_framework.urls')),
     path('user/', include("account.urls")),
     path('member/', include("member.urls")),
